dda91126676e39ca450896917924f33d2f5bf245
pratyush <dakshayani.k@compassitesinc.com>
2014-10-31 15:40:26 +0530

    prettified

--- script.js
+++ script.js
@@ -1,153 +1,153 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	// alert("Hello");
+   // alert("Hello");
 
-	$(".button").mousedown(function(){
-		$(this).css("box-shadow","0px 0px 10px #697875");
-	});
+   $(".button").mousedown(function () {
+      $(this).css("box-shadow", "0px 0px 10px #697875");
+   });
 
-	$(".button").mouseup(function(){
-		$(".button").css("box-shadow","5px 5px 10px #697875");
-	});
+   $(".button").mouseup(function () {
+      $(".button").css("box-shadow", "5px 5px 10px #697875");
+   });
 
-	$("input").keypress(function(){
-		$(this).css("box-shadow","0px 0px 10px #0b3f32");
-	});
+   $("input").keypress(function () {
+      $(this).css("box-shadow", "0px 0px 10px #0b3f32");
+   });
 
-	$("input").blur(function(){
-		$(this).css("box-shadow","0px 0px 10px #aaa");
-	});
+   $("input").blur(function () {
+      $(this).css("box-shadow", "0px 0px 10px #aaa");
+   });
 
-	$(".limit").keypress(function (e) {
-		var key = e.which;
-	 if(key == 13)  // the enter key code
-	 {
-	 	$(".go").click();
-	 	return false;  
-	 }
-	});
+   $(".limit").keypress(function (e) {
+      var key = e.which;
+      if (key == 13) // the enter key code
+      {
+         $(".go").click();
+         return false;
+      }
+   });
 
-	$(".first").keypress(function (e) {
-		var key = e.which;
-	 if(key == 13)  // the enter key code
-	 {
-	 	$(".second").focus();
-	 	return false;  
-	 }
-	});
+   $(".first").keypress(function (e) {
+      var key = e.which;
+      if (key == 13) // the enter key code
+      {
+         $(".second").focus();
+         return false;
+      }
+   });
 
-	$(".second").keypress(function (e) {
-		var key = e.which;
-	 if(key == 13)  // the enter key code
-	 {
-	 	$(".submit").click();
-	 	return false;  
-	 }
-	});
+   $(".second").keypress(function (e) {
+      var key = e.which;
+      if (key == 13) // the enter key code
+      {
+         $(".submit").click();
+         return false;
+      }
+   });
 
-	$(".submit").click(function(){
-		var first = $(".first").val();
-		var second = $(".second").val();
+   $(".submit").click(function () {
+      var first = $(".first").val();
+      var second = $(".second").val();
 
-		var arr = [];
+      var arr = [];
 
-		if (first == "" || second == "") {
-			if (first == "") {
-				$(".first").css("box-shadow","0px 0px 10px red");
-			}
-			if (second == "") {
-				$(".second").css("box-shadow","0px 0px 10px red");
-			}
-			$(".right-container").html("");
-		}else{
+      if (first == "" || second == "") {
+         if (first == "") {
+            $(".first").css("box-shadow", "0px 0px 10px red");
+         }
+         if (second == "") {
+            $(".second").css("box-shadow", "0px 0px 10px red");
+         }
+         $(".right-container").html("");
+      } else {
 
-			// $(".first").val("");
-			// $(".second").val("");
-			$(".right-container").html("<p>The list</p><a class=\"right-handle\" href=\"#\"></a>");
-			$(".first").blur();
-			$(".second").blur();
-			fib(first,second,arr);
-		}
+         // $(".first").val("");
+         // $(".second").val("");
+         $(".right-container").html("<p>The list</p><a class=\"right-handle\" href=\"#\"></a>");
+         $(".first").blur();
+         $(".second").blur();
+         fib(first, second, arr);
+      }
 
-		var str = "";
-		for (var i = 0 ; i <= arr.length - 1; i++) {
-			str = str +"<span>"+arr[i]+"</span>";
-		};
-		$(".right-handle").after(str);
+      var str = "";
+      for (var i = 0; i <= arr.length - 1; i++) {
+         str = str + "<span>" + arr[i] + "</span>";
+      };
+      $(".right-handle").after(str);
 
-	});
+   });
 
-	var fib = function(prime,sec,arr){
-		
-		var temp = 0;
+   var fib = function (prime, sec, arr) {
 
-		if (prime == sec) {
-			arr.push(sec);
-		}
-		
-		temp = prime - sec;
+      var temp = 0;
 
-		arr.push(prime);
+      if (prime == sec) {
+         arr.push(sec);
+      }
 
-		prime = sec;
-		sec = temp;
+      temp = prime - sec;
 
-		if (prime > 0 && sec > 0) {
-			fib(prime,sec,arr);
-		}
+      arr.push(prime);
 
-		
-	};
+      prime = sec;
+      sec = temp;
+
+      if (prime > 0 && sec > 0) {
+         fib(prime, sec, arr);
+      }
 
 
-	$(".go").click(function(){
+   };
 
-		var limit = $(".limit").val();
-		if (limit == "") {
-			$(".limit").css("box-shadow","0px 0px 10px red");
-			$(".left-container").html("");
-		}else{
-			$(".limit").blur();
-			fibo(limit);
-		}
 
-	});
+   $(".go").click(function () {
 
-	var fibo = function(limit){
+      var limit = $(".limit").val();
+      if (limit == "") {
+         $(".limit").css("box-shadow", "0px 0px 10px red");
+         $(".left-container").html("");
+      } else {
+         $(".limit").blur();
+         fibo(limit);
+      }
 
-		var prime = 1;
-		var sec = 1;
-		var arr = [];
+   });
 
-		$(".left-container").html("<p>The list</p><a class=\"left-handle\" href=\"#\"></a>");
-		arr.push(prime);
-		arr.push(sec);
+   var fibo = function (limit) {
 
-		fibonacci(sec,prime,limit,arr);
+      var prime = 1;
+      var sec = 1;
+      var arr = [];
 
-		var str = "";
-		for (var i = arr.length - 1 ; i >= 0 ; i--) {
-			str = str +"<span>"+arr[i]+"</span>";
-		};
-		$(".left-handle").after(str);
+      $(".left-container").html("<p>The list</p><a class=\"left-handle\" href=\"#\"></a>");
+      arr.push(prime);
+      arr.push(sec);
 
-	};
+      fibonacci(sec, prime, limit, arr);
 
-	var fibonacci = function(sec,prime,limit,arr){
+      var str = "";
+      for (var i = arr.length - 1; i >= 0; i--) {
+         str = str + "<span>" + arr[i] + "</span>";
+      };
+      $(".left-handle").after(str);
 
-		var temp = 0;
-		if (limit > 2) {
-			
-			temp = prime + sec;
-			sec = prime;
-			prime = temp;
-			limit = limit - 1;
+   };
 
-			arr.push(prime);
-			fibonacci(sec,prime,limit,arr);
-		};
+   var fibonacci = function (sec, prime, limit, arr) {
 
-	};
+      var temp = 0;
+      if (limit > 2) {
 
-	
+         temp = prime + sec;
+         sec = prime;
+         prime = temp;
+         limit = limit - 1;
+
+         arr.push(prime);
+         fibonacci(sec, prime, limit, arr);
+      };
+
+   };
+
+
 });
