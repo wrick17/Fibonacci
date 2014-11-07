6847ef1b2d61dd74c7ba28fe3253dd9b2ae48938
Pratyush Poddar <pratyush.poddar@compassitesinc.com>
2014-11-07 21:03:48 +0530

    added source files

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['Gruntfile.js', 'js/script.js']
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['css/*.css'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['jshint', 'uglify', 'copy'],
+                options: {
+                    spawn: false,
+                }
+            },
+            css: {
+                files: ['css/*.css'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+        uglify: {
+            all: {
+                files: {
+                    'dist/js/script.min.js': ['js/script.js'],
+                    'dist/js/jquery.min.js': ['js/jquery.js']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "jshint" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'uglify', 'copy', 'watch']);
+
+};
